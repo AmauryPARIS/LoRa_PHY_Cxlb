6bbe63b390edb24124b289f13856fe9f98a6777b
Paul <paul.esteve@inria.fr>
2021-07-30 09:49:38 +0000

    Clean (del lora_dyn_node_strobe & comments) and update automate scripts

--- scripts/launch_node.sh
+++ scripts/launch_node.sh
@@ -1,4 +1,6 @@
 sleep 5
-python2 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/lora_dyn_node.py ${@:2} > ./phy_node.log 2>&1 &
+ARG1="$1"
+shift
+python2 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/lora_dyn_node.py "$@" | tee ./phy_node.log 2>&1 &
 sleep 7
-python3 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/advanced_test/udp_node_less_param.py $1 > ./up_node.log 2>&1
+python3 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/advanced_test/automate/udp_node_less_param.py "$ARG1" | tee ./up_node.log 2>&1
