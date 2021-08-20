7cd2d2b0ae9617077cdcd5165d6f71e2b935eea3
Paul <paul.esteve@inria.fr>
2021-08-20 15:04:38 +0000

    Add script to launch auto upper layer with all param modif

--- scripts/launch_node_modif_all_param.sh
+++ scripts/launch_node_modif_all_param.sh
@@ -7,7 +7,10 @@ cat /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/setpaths.sh >> /root/.bashrc
 env > /root/launch_node_env
 sleep 5
 ARG1="$1"
-shift
+ARG2="$2"
+ARG3="$3"
+ARG4="$4"
+shift 4
 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/lora_dyn_node.py "$@" 2>&1 | tee ./phy_node.log &
 sleep 7
-python3 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/advanced_test/automate/udp_node_less_param.py "$ARG1" "$@" 2>&1 | tee ./up_node.log 
+python3 /root/LoRa_PHY_Cxlb.git/gr-lora_sdr/apps/advanced_test/automate/udp_node_less_param.py "$ARG1" "$ARG2" "$ARG3" "$ARG4" "$@" 2>&1 | tee ./up_node.log 
