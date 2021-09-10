c2558305c07854b73c595cd9f2789c7761febb2f
Paul ESTEVE <paul.esteve@grenoble-inp.org>
2021-09-10 16:20:16 +0200

    Serializing and comparing scripts update

--- gr-lora_sdr/apps/advanced_test/automate/analyze/compare.py
+++ gr-lora_sdr/apps/advanced_test/automate/analyze/compare.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import os, sys, glob
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from seri import *
 
 # To be called with ./compare.py task1 task2...
@@ -10,12 +10,103 @@ def main(argv):
     # os.chdir("/cortexlab/homes/pesteve/results")
     for dir in argv:
         # os.chdir(os.path.join("/cortexlab/homes/pesteve/results", dir))
-        os.chdir(os.path.join("/root", dir))
+        # os.chdir(os.path.join("/root", dir))
 
-        paths = glob.glob("res_*.txt")
+        os.chdir(os.path.join("/Users/estevep/Documents/LoRa_PHY_Cxlb/gr-lora_sdr/apps/advanced_test/automate/analyze", dir))
+        # os.chdir(os.path.join(".", dir))
+        paths = glob.glob("**/res_*.txt", recursive=True)
         l = analyze(paths)
         print("\nNumber of nodes = {}".format(len(l)))
-        print("ACK/N = {}".format(ack_rate(l)))
+        print("Global ACK/N = {}".format(ack_rate(l)))
+        print("--------------------------")
+        SFindex = get_SF_index(l)
+        for SF in SFindex.keys():
+            print("SF = {}".format(SF))
+            l_SF = [ l[i] for i in SFindex[SF] ] # List of all the results for this SF
+            
+            BW_TX_index = get_BW_TX_index(l_SF)
+            for BW_TX in BW_TX_index.keys():
+                print("\tBW.TX = {}".format(BW_TX))
+                l_BW_TX = [ l_SF[i] for i in BW_TX_index[BW_TX] ]
+                
+                print("\t\t{} nodes".format(len(l_BW_TX)))
+                print("\t\tACK/N = {}".format(ack_rate(l_BW_TX)))
+
+def main(argv):
+    """
+    Objectif: return {N: {SF_nb: {SF:[dict1, dict2]}}}
+    """
+
+    fdict = {}
+
+    # os.chdir("/cortexlab/homes/pesteve/results")
+    for dir in argv:
+        # os.chdir(os.path.join("/cortexlab/homes/pesteve/results", dir))
+        # os.chdir(os.path.join("/root", dir))
+
+        os.chdir(os.path.join("/Users/estevep/Documents/LoRa_PHY_Cxlb/gr-lora_sdr/apps/advanced_test/automate/analyze", dir))
+        # os.chdir(os.path.join(".", dir))
+        paths = glob.glob("**/res_*.txt", recursive=True)
+        l = analyze(paths)
+        N = len(l)
+        print("\nNumber of nodes = {}".format(len(l)))
+        print("Global ACK/N = {}".format(ack_rate(l)))
+        print("--------------------------")
+        SFindex = get_SF_index(l)
+        SF_nb = len(SFindex)
+
+        fdict[N]={SF_nb:{}}
+        
+        for SF in SFindex.keys():
+            print("SF = {}".format(SF))
+
+            l_SF = [ l[i] for i in SFindex[SF] ] # List of all the results for this SF
+            # Add the results for this SF in the final dictionary
+            if len(fdict[N][SF_nb]) == 0:
+                fdict[N][SF_nb][SF]=l_SF
+            elif len(fdict[N][SF_nb][SF]) == 0:
+                fdict[N][SF_nb][SF]=l_SF
+            else:
+                fdict[N][SF_nb][SF]+=l_SF
+
+
+
+            # BW_TX_index = get_BW_TX_index(l_SF)
+            # for BW_TX in BW_TX_index.keys():
+            #     print("\tBW.TX = {}".format(BW_TX))
+            #     l_BW_TX = [ l_SF[i] for i in BW_TX_index[BW_TX] ]
+                
+            #     print("\t\t{} nodes".format(len(l_BW_TX)))
+            #     print("\t\tACK/N = {}".format(ack_rate(l_BW_TX)))
+
+    # {N: {SF_nb: {SF:[dict1, dict2]}}}
+    for N, SF_nb_dict in fdict.items():
+        print("{} nodes".format(N))
+        for SF_nb, SF_dict in SF_nb_dict.items():
+            print("\t{} different SF".format(SF_nb))
+            for SF, l_SF in SF_dict.items:
+                print("\t\tSF={}".SF)
+                print("Mean ACK rate: {}".format(ack_rate(l_SF)))
+    
+    return fdict
+
+
+                
+    # print("\t\t{} nodes".format(len(l_SF)))
+    # print("\t\tACK/N = {}".format(ack_rate(l_SF)))
+    
+
+Dico:
+    {N: {SF_nb:[l1, l2]}}
+
+Dico: {SF:{BW:[]}}
+Pour chaque SF:
+    Une liste pour chaque BW:
+
+
+        
+        print("\n")
+        print("_________________________________________________")
         print("\n")
         
 # Tracer M -> sum(ACK)/sum(N) (M)
--- gr-lora_sdr/apps/advanced_test/automate/analyze/seri.py
+++ gr-lora_sdr/apps/advanced_test/automate/analyze/seri.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2
+#/usr/bin/python2
 import os, sys
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-# to be called with ./analyze.py `find /root -name res_*.txt`
+# to be called with ./seri.py `find /cortexlab/homes/[username]/results/task_[task_number] -name res_*.txt`
 
 def analyze(paths):
     """
@@ -10,8 +10,9 @@ def analyze(paths):
     and results (number of transmitted message, number of acknoledgments received) of each node
     """
     M = len(paths) # number of nodes
-    print("Results found: {}".format(paths))
-    print("Number of result files: {}".format(M))
+    if __name__ == '__main__':
+        print("Results found: {}".format(paths))
+        print("Number of result files (nodes): {}".format(M))
 
     l = [{} for i in range(M)]
 
@@ -65,11 +66,51 @@ def analyze(paths):
     return l
 
 def ack_rate(l):
+    """
+    Returns the mean acknoledgment rate for all the nodes in l
+    """
     M = len(l)
+    if M == 0:
+        print("No result in given list")
+        return 0
     Ntot = sum((l[i]["N"] for i in range(M)))
     ACKtot = sum((l[i]["ACK"] for i in range(M)))
-    rate = Ntot/ACKtot
+    rate = float(ACKtot)/Ntot
     return rate
+
+def get_SF_index(l):
+    """
+    Returns a dictionnary with the following structure:
+    {SF:[i, j, ...]} where the integers in the list are the indexes of l such as l[i]["SF"]=SF
+    """
+    M = len(l)
+    if M == 0:
+        print("No result in given list")
+        return {}
+    dict = {}
+    for i in range(M):
+        if l[i]["SF"] in dict:
+            dict[l[i]["SF"]].append(i)
+        else:
+            dict[l[i]["SF"]] = [i]
+    return dict
+
+def get_BW_TX_index(l):
+    """
+    Returns a dictionnary with the following structure:
+    {BW_TX:[i, j, ...]} where the integers in the list are the indexes of l such as l[i]["BW.TX"]=BW_TX
+    """
+    M = len(l)
+    if M == 0:
+        print("No result in given list")
+        return 0
+    dict={}
+    for i in range(M):
+        if l[i]["BW.TX"] in dict:
+            dict[l[i]["BW.TX"]].append(i)
+        else:
+            dict[l[i]["BW.TX"]] = [i]
+    return dict
 
 def main(argv):
     print("ACK/N = {}".format(ack_rate(analyze(argv))))
